809a334a7ddbb645a7b2fd96fb6fc702c69693d6
Andrey Kruglov <kruglov.it@gmail.com>
2021-09-08 21:35:22 +0300

    import common styles and logo

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './app.module.css';
+
+import AppHeader from "../app-header/app-header";
+
+function App() {
+  return (
+    <div >
+      <AppHeader />
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/common.css';
+import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
