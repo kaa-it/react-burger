c644e65a8ca103ef84ee8f0db5083291ff0b2279
Andrey Kruglov <kruglov.it@gmail.com>
2021-09-14 01:42:10 +0300

    load ingredients from server

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,70 +1,78 @@
-import React from 'react';
-import styles from './burger-constructor.module.css';
-import {ingredientPropTypes} from "../../utils/types";
+import React from "react";
+import styles from "./burger-constructor.module.css";
+import { ingredientPropTypes } from "../../utils/types";
 import PropTypes from "prop-types";
-import {Button, ConstructorElement} from "@ya.praktikum/react-developer-burger-ui-components";
-import {DragIcon} from '@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons/drag-icon';
-import {CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons/currency-icon";
+import {
+  Button,
+  ConstructorElement,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { DragIcon } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons/drag-icon";
+import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons/currency-icon";
 
-const ConstructorItem = ({item, type}: any) => {
-    const isLocked = type !== undefined;
-    let suffix = "";
+const ConstructorItem = ({ item, type }: any) => {
+  const isLocked = type !== undefined;
+  let suffix = "";
 
-    if (type === "top") {
-        suffix = " (верх)";
-    } else if (type === "bottom") {
-        suffix = " (низ)";
-    }
+  if (type === "top") {
+    suffix = " (верх)";
+  } else if (type === "bottom") {
+    suffix = " (низ)";
+  }
 
-    return (
-        <div className={styles.constructor_item}>
-            <div style={{visibility: isLocked ? 'hidden' : 'visible'}}>
-                <DragIcon type="primary"/>
-            </div>
-            <ConstructorElement
-                type={type}
-                isLocked={isLocked}
-                text={item.name + suffix}
-                price={item.price}
-                thumbnail={item.image}
-            />
+  return (
+    <div className={styles.constructor_item}>
+      <div style={{ visibility: isLocked ? "hidden" : "visible" }}>
+        <DragIcon type="primary" />
+      </div>
+      <ConstructorElement
+        type={type}
+        isLocked={isLocked}
+        text={item.name + suffix}
+        price={item.price}
+        thumbnail={item.image}
+      />
+    </div>
+  );
+};
+
+const BurgerConstructor = ({ ingredients }: any) => {
+  const bun = ingredients.find((item: any) => item.type === "bun");
+  const notBuns = ingredients.filter((item: any) => item.type !== "bun");
+
+  const total =
+    notBuns.reduce((acc: number, item: any) => item.price + acc, 0) +
+    bun.price * 2;
+
+  return (
+    <div className={`${styles.burger_constructor} pt-25 pl-4 pr-4`}>
+      <div className={styles.constructor_area}>
+        <div className="pr-4">
+          <ConstructorItem type="top" item={bun} />
         </div>
-    );
-}
-
-const BurgerConstructor = ({ingredients} : any) => {
-    const total = ingredients.reduce((acc: number, item: any) => item.price + acc, 0);
-
-    return (
-        <div className={`${styles.burger_constructor} pt-25 pl-4 pr-4`}>
-            <div className={styles.constructor_area}>
-                <div className="pr-4">
-                    <ConstructorItem type="top" item={ingredients[0]}/>
-                </div>
-                <div className={`${styles.scroll_area} custom-scroll`}>
-                    {ingredients.slice(1, -1).map((item: any) => (
-                        <ConstructorItem key={item._id} item={item}/>
-                    ))}
-                </div>
-                <div className="pr-4">
-                    <ConstructorItem type="bottom" item={ingredients[0]}/>
-                </div>
-            </div>
-            <div className={styles.info}>
-                <p className={styles.price}>
-                    {total}
-                    <CurrencyIcon type="primary"/>
-                </p>
-                <Button type="primary" size="large">
-                    Оформить заказ
-                </Button>
-            </div>
+        <div className={`${styles.scroll_area} custom-scroll`}>
+          {notBuns.map((item: any) => (
+            <ConstructorItem key={item._id} item={item} />
+          ))}
         </div>
-    );
+        <div className="pr-4">
+          <ConstructorItem type="bottom" item={bun} />
+        </div>
+      </div>
+      <div className={styles.info}>
+        <p className={styles.price}>
+          {total}
+          <CurrencyIcon type="primary" />
+        </p>
+        <Button type="primary" size="large">
+          Оформить заказ
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 BurgerConstructor.propTypes = {
-    ingredients: PropTypes.arrayOf(ingredientPropTypes.isRequired).isRequired
-}
+  ingredients: PropTypes.arrayOf(ingredientPropTypes.isRequired).isRequired,
+};
 
 export default BurgerConstructor;
