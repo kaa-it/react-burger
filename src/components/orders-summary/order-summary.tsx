011d2e0802d9975228d28a9334375cec74286da7
Andrey Kruglov <kruglov.it@gmail.com>
2021-11-10 01:48:26 +0300

    OrdersSummary

---
+++ src/components/orders-summary/order-summary.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import styles from "./order-summary.module.css";
+
+const nums = [
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+  "034533",
+];
+
+//const nums: string[] = [];
+
+const OrderSummary: React.FC = () => {
+  const cols = Math.floor(nums.length / 10) + 1;
+  const array = Array.from(Array(cols).keys());
+  const chunks = nums.reduce((all: string[][], one, i) => {
+    const ch = Math.floor(i / 10);
+    const s: string[] = [];
+    all[ch] = s.concat(all[ch] || [], one);
+    return all;
+  }, []);
+  return (
+    <div className={styles.order_summary}>
+      <div className={styles.statuses}>
+        <div className={styles.status}>
+          <span className="text_type_main-medium pb-6">Готовы:</span>
+          <div className={styles.columns_success}>
+            {nums.length > 0 &&
+              array.map((i) => (
+                <div className={styles.column}>
+                  {chunks[i].map((v) => (
+                    <span className="text_type_digits-default">{v}</span>
+                  ))}
+                </div>
+              ))}
+          </div>
+        </div>
+        <div className={styles.status}>
+          <span className="text_type_main-medium pb-6">В работе:</span>
+          <div className={styles.columns}>
+            {nums.length > 0 &&
+              array.map((i) => (
+                <div className={styles.column}>
+                  {chunks[i].map((v) => (
+                    <span className="text_type_digits-default">{v}</span>
+                  ))}
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+      <div className={styles.count}>
+        <span className="text_type_main-medium">Выполнено за все время:</span>
+        <span className="text_type_digits-large">28752</span>
+      </div>
+      <div className={styles.count}>
+        <span className="text_type_main-medium">Выполнено за сегодня:</span>
+        <span className="text_type_digits-large">138</span>
+      </div>
+    </div>
+  );
+};
+
+export default OrderSummary;
--- src/pages/order-feed/order-feed.tsx
+++ src/pages/order-feed/order-feed.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./order-feed.module.css";
 import Orders from "../../components/orders/orders";
+import OrderSummary from "../../components/orders-summary/order-summary";
 
 const OrderFeedPage: React.FC = () => {
   return (
@@ -8,6 +9,9 @@ const OrderFeedPage: React.FC = () => {
       <div className={styles.orders}>
         <p className={styles.title}>Лента заказов</p>
         <Orders />
+      </div>
+      <div className={styles.summary}>
+        <OrderSummary />
       </div>
     </div>
   );
