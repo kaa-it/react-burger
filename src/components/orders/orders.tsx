827ef0d7673aebb57ceea6cabe9d27eaf999474a
Andrey Kruglov <kruglov.it@gmail.com>
2021-11-09 23:39:05 +0300

    orders to OrderFeedPage

--- src/components/orders/orders.tsx
+++ src/components/orders/orders.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import styles from "./orders.module.css";
+import OrderCard from "../order-card/order-card";
 
 const Orders: React.FC = () => {
   return (
     <div className={styles.orders}>
-      <span className="text_type_main-default">В разработке...</span>
+      <p className={styles.title}>Лента заказов</p>
+      <div className={styles.feed}>
+        <ul className={`${styles.list} custom-scroll`}>
+          <li>
+            <OrderCard />
+          </li>
+          <li>
+            <OrderCard />
+          </li>
+          <li>
+            <OrderCard />
+          </li>
+          <li>
+            <OrderCard />
+          </li>
+          <li>
+            <OrderCard />
+          </li>
+          <li>
+            <OrderCard />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/pages/order-feed/order-feed.tsx
+++ src/pages/order-feed/order-feed.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./order-feed.module.css";
 import OrderCard from "../../components/order-card/order-card";
+import Orders from "../../components/orders/orders";
 
 const OrderFeedPage: React.FC = () => {
   return (
     <div className={styles.order_feed}>
-      <OrderCard />
+      <Orders />
     </div>
   );
 };
